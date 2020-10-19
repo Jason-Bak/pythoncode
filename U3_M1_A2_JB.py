719523f4cd5f6c196154e7267896ea27cc28ced8
Jason-Bak <56934929+Jason-Bak@users.noreply.github.com>
2020-10-19 10:10:50 -0400

    Create U3_M1_A2_JB.py

---
+++ U3_M1_A2_JB.py
@@ -0,0 +1,56 @@
+#program name: U3_M1_A1_JB
+#program purpose: learn about assigning and printing variables
+#created/revised: J.Bak on 10-19-20
+
+
+
+#step 8
+print("Welcome to Programming Python Class.")
+#step 9
+print('Welcome to AIT High School, we are so glad to have you.')
+#step 11
+print(15)
+#step 12 
+print('2020')
+#step 14
+print('I am a string')
+#step 15
+thisMsg = 'I am a string'
+#step 16
+print(thisMsg)
+#step 17 
+myMessage = 'I am learning to write code using Python 3'
+#step 18 
+print(myMessage)
+#step 20
+name = 'Josiah Jones'
+#step 21
+print(name)
+#step 22
+testValue = 22
+#step 23 
+print(testValue)
+#step 24
+testValue = 'Brittany'
+#step 25
+print(testValue)
+#step 26
+studentName = 'Jason Bak'
+#step 27
+print(studentName)
+#step 28
+studentName = 'Cory Zhou'
+#step 29
+print(studentName)
+#step 30
+studentName = 'William Kim'
+#step 31
+print(studentName)
+#step 32
+bucket = 18
+#step 33
+print(bucket)
+#step 34
+print('123')
+#step 35
+print(123)
